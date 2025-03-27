09b026f43ff63370920d19169f94013b0c423ec3
eunsung222 <greeejd1@gmail.com>
2025-03-27 14:52:12 +0900

    Add files via upload

---
+++ untitled/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+//        final String Name = "Kim";
+//        final int Age = 22;
+//        System.out.println(Name + "님은 " + Age + "살 입니다.");
+        final double PI = 3.14;
+        final double r = 10;
+        final double S = r * r * PI;
+
+        System.out.println("넓이 = " + S);
+    }
+}
